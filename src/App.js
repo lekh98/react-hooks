270525f4ae0ba1f1e075d6216af667a46981692b
lekhnath <lekhnath.yadav1991@gmail.com>
2024-05-12 17:02:57 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MemoHook from './components/usememo/MemoHook';
+//import LayoutEffect from './components/uselayouteffect/LayoutEffect';
+//import RefHook from './components/useref/RefHook';
+//import RefHook1 from './components/useref/RefHook1';
+//import ReducerHook from './components/usereducer/ReducerHook';
+//import ClearUp from './components/useEffect/ClearUp';
+//import Parent from './parent';
+//import UseEffect from './components/useEffect/UseEffect';
+//import UseStateObject from './components/useState/UseStateObject';
+//import UseState from './components/useState/UseState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MemoHook></MemoHook>
   );
 }
 
